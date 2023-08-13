4f0da48cf7d9d448707cb7d5fafc5c227f90c09d
Petr Ankudinov <petr.ankudinov@gmail.com>
2023-08-13 18:57:47 +0000

    test with concat

--- avd-containers/universal/.devcontainer/postCreateCommand.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pip3 install "ansible-core>=2.13.1,<2.14.0" \
-&& ansible-galaxy collection install arista.avd:==${AVD_VERSION} \
-&& pip3 install -r /home/vscode/.ansible/collections/ansible_collections/arista/avd/requirements.txt
